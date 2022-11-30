b001b82a0dc7ce7467aa405658e5760c49872cc8
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-11-29 17:04:21 -0800

    Added useState hooks for form submit functionality

--- src/Components/NewUser/AddUserForm.js
+++ src/Components/NewUser/AddUserForm.js
@@ -1,22 +1,52 @@
-import React from 'react';
-import './AddUserForm.css';
+import React, { useState } from "react";
+import "./AddUserForm.css";
 
 const AddUserForm = () => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const userData = {
+      username: enteredUsername,
+      age: enteredAge
+    }
+
+    console.log('Entered userData:', userData);
+    setEnteredUsername('');
+    setEnteredAge('');
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-user__controls">
         <div className="new-user__control">
           <label>Username</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
         </div>
         <div className="new-user__control">
           <label>Age (Years)</label>
-          <input type="text" />
+          <input type="text" value={enteredAge} onChange={ageChangeHandler} />
         </div>
       </div>
-      <button className="new-user__button" type="button">Add User</button>
+      <button className="new-user__button" type="submit" >
+        Add User
+      </button>
     </form>
   );
-}
+};
 
 export default AddUserForm;
