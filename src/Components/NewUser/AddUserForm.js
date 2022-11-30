534fe156d800dda9754a68019c2f1b4c2dec77ec
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-11-29 16:31:54 -0800

    Added styling to AddUserForm

--- src/Components/NewUser/AddUserForm.js
+++ src/Components/NewUser/AddUserForm.js
@@ -4,19 +4,19 @@ import './AddUserForm.css';
 const AddUserForm = () => {
   return (
     <form>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
+      <div className="new-user__controls">
+        <div className="new-user__control">
           <label>Username</label>
           <input type="text" />
         </div>
       </div>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
+      <div className="new-user__controls">
+        <div className="new-user__control">
           <label>Age (Years)</label>
           <input type="text" />
         </div>
       </div>
-      <button type="button">Add User</button>
+      <button className="new-user__button" type="button">Add User</button>
     </form>
   );
 }
